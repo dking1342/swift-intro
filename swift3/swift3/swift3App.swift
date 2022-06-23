3f86d4a279ab3d84dea36180ad576d18020563ad
David <leanmachine42@yahoo.com>
2022-06-22 22:54:35 -0500

    init commit and config

---
+++ swift3/swift3/swift3App.swift
@@ -0,0 +1,17 @@
+//
+//  swift3App.swift
+//  swift3
+//
+//  Created by David King on 6/22/22.
+//
+
+import SwiftUI
+
+@main
+struct swift3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
