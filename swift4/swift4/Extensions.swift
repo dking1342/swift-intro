b4f77b92cf172b0f4f783134e9c66b477a220185
David <leanmachine42@yahoo.com>
2022-06-23 23:26:36 -0500

    created color scheme and worked with home view

---
+++ swift4/swift4/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  swift4
+//
+//  Created by David King on 6/23/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let text = Color("Text")
+    static let icon = Color("Icon")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
