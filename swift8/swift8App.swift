a9f15dd6e31df085143001b5fe33f4459c8f89cb
David <leanmachine42@yahoo.com>
2022-06-30 15:33:35 -0500

    Initial Commit

---
+++ swift8/swift8App.swift
@@ -0,0 +1,17 @@
+//
+//  swift8App.swift
+//  swift8
+//
+//  Created by David King on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct swift8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
