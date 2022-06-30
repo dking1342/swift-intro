2e33e47b812106ebb57aa9e04720b40c36c7d6bc
David <leanmachine42@yahoo.com>
2022-06-29 20:29:02 -0500

    Initial Commit

---
+++ swift6/swift6App.swift
@@ -0,0 +1,17 @@
+//
+//  swift6App.swift
+//  swift6
+//
+//  Created by David King on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct swift6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
