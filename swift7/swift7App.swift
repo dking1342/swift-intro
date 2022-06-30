a39865b15f896efe8dfabc7973c0a30f68ef8902
David <leanmachine42@yahoo.com>
2022-06-29 23:38:37 -0500

    Initial Commit

---
+++ swift7/swift7App.swift
@@ -0,0 +1,17 @@
+//
+//  swift7App.swift
+//  swift7
+//
+//  Created by David King on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct swift7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
